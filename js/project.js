dbe25a8b918f2b3a4a52f991d632e3ea8c0586b3
kuangkeng <kuangkeng@gmail.com>
2019-11-12 15:05:11 +0800

    video display/hide changed using JS

--- js/project.js
+++ js/project.js
@@ -153,7 +153,7 @@ $(document).ready(function() {
 
     if (width<500){
         //show only mobile video
-        $("#video-desktop").hide();
+        $("#video-video").hide();
 
         //For chart1a mobile
         $('#chart-container-1a').on("touchmove", function(e) {       
